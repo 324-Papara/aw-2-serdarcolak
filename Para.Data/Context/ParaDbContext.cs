21f62b84b43dee3af0d412ae97775a434ccc6107
Serdar Çolak <serdarcolak.99@hotmail.com>
2024-07-14 21:04:38 +0300

    FluentValidation-Include-Where

---
+++ Para.Data/Context/ParaDbContext.cs
@@ -0,0 +1,26 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Para.Base.Entity;
+using Para.Data.Configuration;
+using Para.Data.Domain;
+
+namespace Para.Data.Context;
+
+public class ParaDbContext : DbContext
+{
+    public ParaDbContext(DbContextOptions<ParaDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Customer> Customers { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+        modelBuilder.ApplyConfiguration(new CustomerDetailConfiguration());
+        modelBuilder.ApplyConfiguration(new CustomerAddressConfiguration());
+        modelBuilder.ApplyConfiguration(new CustomerPhoneConfiguration());
+        
+    }
+}
